2e128598697fedd5211847595c0d72fbd633e362
oktober <5934988+oktober@users.noreply.github.com>
2023-01-24 10:57:03 -0800

    Moved all Feed route to a group

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\FeedsController;
 use App\Http\Controllers\EntriesController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,28 +16,21 @@ use App\Http\Controllers\EntriesController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 //Shows home page
 Route::get('/', [HomeController::class, 'index']);
 
-//Shows all feeds listed out - needs formatting
-Route::get('/feeds', [FeedsController::class, 'index']);
-//Shows form to link to a new feed
-Route::get('/feeds/create', [FeedsController::class, 'create']);
-//Handles the POST from /feeds/create to store the new feed
-Route::post('/feeds', [FeedsController::class, 'store']);
-//Shows one feed and all its entries
-Route::get('/feeds/{feed}', [FeedsController::class, 'show'])->name('feeds.show');
-//Shows form to edit feed details
-Route::get('/feeds/{feed}/edit', [FeedsController::class, 'edit']);
-//Handles PUT from /feeds/{feed}/edit to update the feed details
-Route::put('/feeds/{feed}', [FeedsController::class, 'update']);
-//Handles DELETE from /feeds/{feed}/edit to delete feed record & all entries
-Route::delete('/feeds/{feed}', [FeedsController::class, 'destroy']);
+// Create a group for all the Feeds controllers
+Route::controller(FeedsController::class)->group(function () {
+    Route::get('/feeds', 'index');
+    Route::get('/feeds/create', 'create');
+    Route::post('/feeds', 'store');
+    Route::get('/feeds/{feed}', 'show')->name('feeds.show');
+    Route::get('/feeds/{feed}/edit', 'edit');
+    Route::put('/feeds/{feed}', 'update');
+    Route::delete('/feeds/{feed}', 'destroy');
+});
 
-//Shows all entries - will be good to sort by date and adding pagination to only show the X most recent
+//Shows all entries 
+// TODO: sort by date and add pagination to only show the X most recent
 //Route::get('/entries', [EntriesController::class, 'index']);
 Route::get('/entry/{entry}', [EntriesController::class, 'show']);
