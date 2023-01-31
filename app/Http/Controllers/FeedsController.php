172f60c34e05891cbe954b808a1bf3fbb3112d8b
oktober <5934988+oktober@users.noreply.github.com>
2023-01-31 08:50:26 -0800

    Fixed formatting and removed unused classes

--- app/Http/Controllers/FeedsController.php
+++ app/Http/Controllers/FeedsController.php
@@ -5,30 +5,30 @@ namespace App\Http\Controllers;
 use App\Models\Feed;
 use App\Models\Entry;
 use App\Services\Feed as ServicesFeed;
-use Carbon\Carbon;
-use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Http;
 
 class FeedsController extends Controller
 {
-    public function index(){
+    public function index()
+    {
     	$feeds = Feed::get();
     	
     	return view('feeds.index', ['feeds' => $feeds]);
     }
 
-    public function create(){
+    public function create()
+    {
     	return view('feeds.create');
     }
 
-    public function store(){
+    public function store()
+    {
         // Validate the input fields
     	$this->validateFeed();
 
         $feed = new ServicesFeed;
         $feedFound = $feed->find(request('site_url'));
 
-        //If we were able to find an RSS feed file
+        // If we were able to find an RSS feed file
         if ($feedFound) {
             $typeOfXML = $feed->getXMLType();
             $feedDetails = $feed->feedDetails($typeOfXML);
@@ -61,31 +61,31 @@ class FeedsController extends Controller
         }
     }
 
-    public function show(Feed $feed){
-    	
+    public function show(Feed $feed)
+    {
     	return view('feeds.show', ['feed' => $feed]);
     }
 
-    public function edit(Feed $feed){
-    	// Show a view to edit a feed
-
+    public function edit(Feed $feed)
+    {
     	return view('feeds.edit', ['feed' => $feed]);
     }
 
-    public function update(Feed $feed){
+    public function update(Feed $feed)
+    {
     	$this->validateFeed();
 
     	$currentTime = new \DateTime();
 
     	$feed->update([
-            // only need this line if we're allowing them to update the feed/XML URL
+            // Only need this line if we're allowing them to update the feed/XML URL
     		// 'feed_url' => request('feed_url'),
     		'site_url' => request('site_url'),
     		'site_title' => request('site_title'),
     		'last_updated' => $currentTime->format('Y-m-d H:i:s')
     	]);
 
-        // Get XML file from feed_url
+        // TODO: Get XML file from feed_url
         //walk through it and use the feeds to update the entries table
             //if entry doesn't exist 
                 //insert into entries
@@ -96,13 +96,15 @@ class FeedsController extends Controller
     	return redirect(route('feeds.show', $feed));
     }
 
-    public function destroy(Feed $feed){
+    public function destroy(Feed $feed)
+    {
     	$feed->delete();
     	
     	return redirect('/')->with('success', 'Feed has been successfully deleted');
     }
 
-    protected function validateFeed() {
+    protected function validateFeed()
+    {
         // TODO: Shouldn't the validation be in the model?
         // And this function should probably just go inline under store()
     	return request()->validate([
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Feed;
 
 class HomeController extends Controller
 {
-    public function index() {
-		$feeds = Feed::with('entries')->get();
+  public function index()
+  {
+    $feeds = Feed::with('entries')->get();
 
-    	return view('home', ['feeds' => $feeds]);
-    }
+    return view('home', ['feeds' => $feeds]);
+  }
 }
