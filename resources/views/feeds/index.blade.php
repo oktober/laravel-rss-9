c4066a918ab5402bc373554c8d7451d9bd811e44
oktober <5934988+oktober@users.noreply.github.com>
2023-02-02 13:53:01 -0800

    Formatted list of feeds page

--- resources/views/feeds/index.blade.php
+++ resources/views/feeds/index.blade.php
@@ -1,11 +1,25 @@
 @extends ('template')
 
 @section ('content')
-	<h1>Feeds</h1>
+    <section id="three" class="wrapper">
+        <div class="inner">
+			<h1>Feeds</h1>
+		</div>
 
-	@foreach ($feeds as $feed)
-	<div>{{ $feed->site_title}} </div>
-	<div>{{ $feed->site_url}} </div>
-	<div>{{ $feed->last_updated}} </div>
-	@endforeach
+        <div class="inner">
+			@if ($feeds->isNotEmpty())
+				@foreach ($feeds as $feed)
+				<h2>
+					<a href="/feeds/{{ $feed->id }}">
+						{{ $feed->site_title}}
+					</a>
+				</h2>
+				<p>{{ $feed->site_url}}</p>
+				<p>Last updated: {{ $feed->updated_at}}</p>
+				@endforeach
+			@else
+				<h2>No feeds found</h2>
+			@endif
+		</div>
+	</section>
 @endsection
