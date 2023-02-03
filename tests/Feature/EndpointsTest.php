e2473fb58591f86a2bdad95150e174f7c4c28199
oktober <5934988+oktober@users.noreply.github.com>
2023-02-03 15:35:39 -0800

    Refactored asserts and added assertViewIs

--- tests/Feature/EndpointsTest.php
+++ tests/Feature/EndpointsTest.php
@@ -15,16 +15,19 @@ class EndpointsTest extends TestCase
     public function test_that_all_endpoints_work()
     {
         $response = $this->get('/');
-        $response->assertStatus(200);
+        $response->assertOk();
         $response->assertSee('Blog Feed');
+        $response->assertViewIs('home');
 
         $response = $this->get('/feeds');
-        $response->assertStatus(200);
+        $response->assertOk();
         $response->assertSee('No feeds found');
+        $response->assertViewIs('feeds.index');
 
         $response = $this->get('/feeds/create');
-        $response->assertStatus(200);
+        $response->assertOk();
         $response->assertSee('New Feed');
+        $response->assertViewIs('feeds.create');
 
         $response = $this->get('/feeds/1');
         $response->assertStatus(404);
@@ -37,10 +40,12 @@ class EndpointsTest extends TestCase
             'feed_id' => $feed->id
         ]);
         $response = $this->get('/feeds/1');
-        $response->assertStatus(200);
+        $response->assertOk();
+        $response->assertViewIs('feeds.show');
 
         $response = $this->get('/entry/1');
-        $response->assertStatus(200);
+        $response->assertOk();
+        $response->assertViewIs('entries.show');
 
         $response = $this->get('/random');
         $response->assertStatus(404);
