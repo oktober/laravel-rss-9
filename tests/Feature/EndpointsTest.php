6b4e2416b7cfefcaecdf23bc519e4ec811021018
oktober <5934988+oktober@users.noreply.github.com>
2023-02-03 16:47:02 -0800

    Fixed logic and added comments

--- tests/Feature/EndpointsTest.php
+++ tests/Feature/EndpointsTest.php
@@ -14,21 +14,25 @@ class EndpointsTest extends TestCase
 
     public function test_that_all_endpoints_work()
     {
+        // home page is up and showing the right view
         $response = $this->get('/');
         $response->assertOk();
         $response->assertSee('Blog Feed');
         $response->assertViewIs('home');
 
+        // /feeds page is up and showing the right view
         $response = $this->get('/feeds');
         $response->assertOk();
         $response->assertSee('No feeds found');
         $response->assertViewIs('feeds.index');
 
+        // /feeds/create page is up and showing the right view
         $response = $this->get('/feeds/create');
         $response->assertOk();
         $response->assertSee('New Feed');
         $response->assertViewIs('feeds.create');
 
+        // there are no feeds or entries yet
         $response = $this->get('/feeds/1');
         $response->assertStatus(404);
         $response = $this->get('/entry/1');
@@ -39,14 +43,19 @@ class EndpointsTest extends TestCase
         Entry::factory(3)->create([
             'feed_id' => $feed->id
         ]);
-        $response = $this->get('/feeds/1');
+        $response = $this->get('/feeds/' . $feed->id);
+        // this feed has been created and is showing the right view
         $response->assertOk();
         $response->assertViewIs('feeds.show');
 
-        $response = $this->get('/entry/1');
+        // get the first entry from this feed
+        $entry = Entry::where('feed_id', $feed->id)->first();
+        $response = $this->get('/entry/' . $entry->id);
+        // this entry has been created and is showing the right view
         $response->assertOk();
         $response->assertViewIs('entries.show');
 
+        // unassigned routes are showing 404
         $response = $this->get('/random');
         $response->assertStatus(404);
     }
