b5cdd1dd25ae62a6e2755ff4936558de68550d2c
oktober <5934988+oktober@users.noreply.github.com>
2023-02-23 16:48:06 -0800

    Install Breeze package for authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\FeedsController;
 use App\Http\Controllers\EntriesController;
@@ -11,8 +12,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -34,3 +35,15 @@ Route::controller(FeedsController::class)->group(function () {
 // TODO: sort by date and add pagination to only show the X most recent
 //Route::get('/entries', [EntriesController::class, 'index']);
 Route::get('/entry/{entry}', [EntriesController::class, 'show']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
