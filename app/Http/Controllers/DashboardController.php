534cb09d4e7e09dc009fa12623fd74e296785fc7
oktober <5934988+oktober@users.noreply.github.com>
2023-03-07 10:21:00 -0800

    Add logic to only grab the feeds for this user

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Feed;
+use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
   public function index()
   {
-    $feeds = Feed::with('entries')->get();
+    // show all feeds that belong to this user that have entries
+    $feeds = Feed::whereBelongsTo(request()->user())->with('entries')->get();
 
     return view('dashboard', ['feeds' => $feeds]);
   }
