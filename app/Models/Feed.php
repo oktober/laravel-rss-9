9b7b90b85a6b0f6e4c0abc26ac4ac44a10804330
oktober <5934988+oktober@users.noreply.github.com>
2023-03-07 10:20:06 -0800

    Add relationship to User and Feed models to connect them

--- app/Models/Feed.php
+++ app/Models/Feed.php
@@ -10,10 +10,15 @@ class Feed extends Model
     use HasFactory;
     
 	protected $fillable = [
+		'user_id', 
 		'feed_url', 
 		'site_url', 
 		'site_title'
 	];
+
+    public function user() {
+		return $this->belongsTo(User::class);
+    }
 
     public function entries(){
     	return $this->hasMany(Entry::class);
