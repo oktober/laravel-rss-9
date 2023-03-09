92b8805659f7ebb9e17fa6ca9c01e603f6db0fc5
oktober <5934988+oktober@users.noreply.github.com>
2023-03-09 09:53:34 -0800

    Add feed links to the hamburger dropdown menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -76,6 +76,12 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('feeds')" :active="request()->routeIs('feeds')">
+                {{ __('Feeds') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('feeds.create')" :active="request()->routeIs('feeds.create')">
+                {{ __('Add New Feed') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
