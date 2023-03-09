aa213a9de989ad57c01be8a340deeb33e448c8e5
oktober <5934988+oktober@users.noreply.github.com>
2023-03-09 14:18:21 -0800

    Update styles for error messages and save buton on create feed view

--- resources/views/feeds/create.blade.php
+++ resources/views/feeds/create.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight min-w-max">
         {{ __("Add New Feed") }}
         </h2>
     </x-slot>
@@ -13,33 +13,28 @@
                     <form method="POST" action="{{ route('feeds.store') }}">
                         @csrf
 
-                        <div>
-                            <label class="inline-block font-semibold text-lg whitespace-nowrap" for="site_url">Site URL</label>
-                            <div class="relative mt-2 rounded-md shadow-sm w-full max-w-2xl">
-                                <input 
-                                    class="block w-full max-w-2xl rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                    type="text" 
-                                    name="site_url" 
-                                    id="site_url"
-                                    value="{{ old('site_url') }}"
-                                    maxlength="255"
-                                >
+                        <div class="min-w-max">
+                            <div>
+                                <label class="inline-block font-semibold text-lg whitespace-nowrap" for="site_url">Enter a site URL</label>
+                                <div class="relative mt-2 rounded-md shadow-sm w-full max-w-2xl">
+                                    <input 
+                                        class="block w-full max-w-2xl rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 placeholder:italic sm:text-sm sm:leading-6"
+                                        type="text" 
+                                        name="site_url" 
+                                        id="site_url"
+                                        placeholder="e.g. https://test.com"
+                                        value="{{ old('site_url') }}"
+                                        maxlength="255"
+                                        required
+                                    >
+                                </div>
+                                <!-- To show any validation or session errors -->
+                                <p class="text-sm text-red-500 mt-2">{{ $errors->first('site_url') ?? '' }}{{ session('error') ?? '' }}</p>
                             </div>
-                            @error('site_url')
-                                <p class="">{{ $errors->first('site_url') }}</p>
-                            @enderror
-                            @if (session('error'))
-                                <p class="">{{ session('error') }}</p>
-                            @endif
-                        </div>
 
-                        <div class="mt-4">
-                            <button 
-                                type="submit" 
-                                class="group relative rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            >
-                            Create
-                            </button>
+                            <div class="mt-4 flex justify-end max-w-2xl">
+                                <x-primary-button>{{ __('Add Feed') }}</x-primary-button>
+                            </div>
                         </div>
                     </form>
                 </div>
