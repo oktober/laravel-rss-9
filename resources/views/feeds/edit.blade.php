57a1e7e8af073c0f148a6a08b108561076142603
oktober <5934988+oktober@users.noreply.github.com>
2023-03-09 14:20:10 -0800

    Rename 'Site Title' to 'Feed Name' on feed edit view

--- resources/views/feeds/edit.blade.php
+++ resources/views/feeds/edit.blade.php
@@ -15,7 +15,7 @@
                         @method('PUT')
 
                         <div class="min-w-fit">
-                            <label class="inline-block font-semibold text-lg whitespace-nowrap" for="site_title">Site Title</label>
+                            <label class="inline-block font-semibold text-lg whitespace-nowrap" for="site_title">Feed Name</label>
                                 <input 
                                     class="block w-full max-w-2xl rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
                                     type="text" 
